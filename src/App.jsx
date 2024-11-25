c80028d6faa37a258bc11686818756ecce9c3381
Marcus-Vi <152079038+Marcus-Vi@users.noreply.github.com>
2024-11-25 12:39:59 -0300

    Corrigida a capitalização de Task

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Task from "./components/task";
+import Task from "./components/Task";
 import AddTask from "./components/AddTask";
 import { v4 } from "uuid";
 
@@ -11,17 +11,6 @@ function App() {
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
-
-  useEffect(() => {
-    async function EffectTask() {
-      const response = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=10");
-      const data = await response.json();
-      console.log(data);
-      setTasks(data)
-    }
-    // EffectTask();
-
-  }, []);
 
   function onTaskClick(taskid) {
     const newTask = tasks.map((task) => {
